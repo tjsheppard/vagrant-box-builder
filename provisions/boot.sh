64118a4880ac4a38b567b3e851f84a2455c9dba7
Tom Sheppard <sheppardnexus@gmail.com>
2018-10-31 16:49:48 +0000

    Database creation + some changes

--- provisions/boot.sh
+++ provisions/boot.sh
@@ -2,4 +2,5 @@ sudo systemctl start firewalld
 sudo systemctl start httpd
 sudo systemctl start mariadb
 sudo yum update -y
-echo -e "\n$1 is ready for development! 🦖\nSITE $1.local\nIP ADDRESS: $(hostname -I)\n"
+echo -e "\n\n$1 was built! If this is your first time do 'vagrant reload' to restart the box and get ready to develop 🦕"
+echo -e "\nSITE $1.local\nIP ADDRESS: $(hostname -I) 🦖\n"
--- provisions/centos.sh
+++ provisions/centos.sh
@@ -27,7 +27,7 @@ sudo firewall-cmd --reload
 echo -e "\n####### 4. PHP ################################\n"
 sudo yum-config-manager --enable remi-php72
 sudo yum install php -y
-sudo yum install php-mysqlnd php-mysql php-fpm php-mbstring php-intl php-simplexml php-zip php-openssl php-pdo php-tokenizer php-xml php-ctype php-json -y
+sudo yum install php-mysqlnd php-mysql php-mcrypt php-fpm php-mbstring php-intl php-simplexml php-zip php-openssl php-pdo php-tokenizer php-xml php-ctype php-json -y
 
 echo -e "\n####### 5. APACHE #############################\n"
 sudo systemctl start httpd
@@ -38,10 +38,14 @@ sudo chmod -R 777 /etc/httpd/conf.d
 
 sudo echo -e "<VirtualHost *:80>\n\tServerAdmin tom@$1.local\n\tServerName $1.local\n\tServerAlias www.$1.local\n\tDocumentRoot /var/www/$1.local\n\tErrorLog /var/www/logs/error.log\n\tCustomLog /var/www/logs/access.log combined\n\t<Directory "/var/www/$1.local">\n\t\tOrder allow,deny\n\t\tAllow from all\n\t\tRequire all granted\n\t\tAllowOverride All\n\t</Directory>\n</VirtualHost>" >> /etc/httpd/conf.d/000-$1.local.conf
 
-sudo echo -e "<VirtualHost *:443>\n\tServerAdmin tom@$1.local\n\tServerName $1.local\n\tServerAlias www.$1.local\n\tDocumentRoot /var/www/$1.local\n\tErrorLog /var/www/logs/error-ssl.log\n\tCustomLog /var/www/logs/access-ssl.log combined\n\t<Directory "/var/www/$1.local">\n\t\tOrder allow,deny\n\t\tAllow from all\n\t\tRequire all granted\n\t\tAllowOverride All\n\t</Directory>\n\tSSLEngine on\n\tSSLCertificateFile /var/www/ssl/server.crt\n\tSSLCertificateKeyFile /var/www/ssl/server.key\n</VirtualHost>" >> /etc/httpd/conf.d/000-ssl-$1.local.conf
-
 sudo chmod -R 777 /etc/httpd/conf.d/000-$1.local.conf
 sudo chown root:root /etc/httpd/conf.d/000-$1.local.conf
+
+sudo echo -e "<VirtualHost *:443>\n\tServerAdmin tom@$1.local\n\tServerName $1.local\n\tServerAlias www.$1.local\n\tDocumentRoot /var/www/$1.local\n\tErrorLog /var/www/logs/error-ssl.log\n\tCustomLog /var/www/logs/access-ssl.log combined\n\t<Directory "/var/www/$1.local">\n\t\tOrder allow,deny\n\t\tAllow from all\n\t\tRequire all granted\n\t\tAllowOverride All\n\t</Directory>\n\tSSLEngine on\n\tSSLCertificateFile /var/www/ssl/server.crt\n\tSSLCertificateKeyFile /var/www/ssl/server.key\n</VirtualHost>" >> /etc/httpd/conf.d/000-ssl-$1.local.conf
+
+sudo chmod -R 777 /etc/httpd/conf.d/000-ssl-$1.local.conf
+sudo chown root:root /etc/httpd/conf.d/000-ssl-$1.local.conf
+
 sudo echo -e "To Infinity and beyond! Now just add '$(hostname -I)   $1.local' to your hosts file 🚀" >> /var/www/$1.local/index.html
 sudo systemctl restart httpd
 
@@ -68,11 +72,17 @@ y
 y
 EOF
 
+mysql -uroot -pvagrant<<MYSQL_SCRIPT
+CREATE DATABASE $1;
+GRANT ALL PRIVILEGES ON $1.* TO 'root'@'localhost';
+FLUSH PRIVILEGES;
+MYSQL_SCRIPT
+
 echo -e "\n####### 8. NODE.JS ##############################\n"
 sudo curl -sL https://rpm.nodesource.com/setup_8.x | sudo bash -
 sudo yum install nodejs -y
 
-echo -e "\n####### 8. SSL ##############################\n"
+echo -e "\n####### 9. SSL ##############################\n"
 sudo mkdir /var/www/ssl
 sudo chmod -R 777 /var/www/ssl
 cd /var/www/ssl
@@ -88,8 +98,6 @@ openssl req -x509 -new -nodes -key rootCA.key -sha256 -days 1024 -passin pass:$1
 openssl req -new -sha256 -nodes -out server.csr -newkey rsa:2048 -keyout server.key -config <( cat server.csr.cnf )
 
 openssl x509 -req -in server.csr -CA rootCA.pem -CAkey rootCA.key -CAcreateserial -out server.crt -passin pass:$1 -days 500 -sha256 -extfile v3.ext
-
-echo -e "\n$1 was built! Now 'vagrant reload' to restart the box and get ready to develop 🦕\n"
 
 # CREATE DATABASE
 # XDEBUG
