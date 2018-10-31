8a4c0e31df4759c8b276a2d0a8349a280794f948
Tom Sheppard <sheppardnexus@gmail.com>
2018-10-31 12:37:33 +0000

    SSL added

--- provisions/centos.sh
+++ provisions/centos.sh
@@ -35,7 +35,11 @@ sudo systemctl enable httpd
 sudo mkdir /var/www/$1.local/
 sudo mkdir /var/www/logs/
 sudo chmod -R 777 /etc/httpd/conf.d
-sudo echo -e "<VirtualHost *:80>\n\tServerAdmin tom@$1.local\n\tServerName $1.local\n\tServerAlias www.$1.local\n\tDocumentRoot /var/www/$1.local\n\tErrorLog /var/www/logs/error.log\n\tCustomLog /var/www/logs/access.log combined\n\t<Directory "/var/www/$1.local">\n\t\tAllowOverride\n\t\tOrder allow,deny\n\t\tAllow from all\n\t\tRequire all granted\n\t\tAllowOverride All\n\t</Directory>\n</VirtualHost>" >> /etc/httpd/conf.d/000-$1.local.conf
+
+sudo echo -e "<VirtualHost *:80>\n\tServerAdmin tom@$1.local\n\tServerName $1.local\n\tServerAlias www.$1.local\n\tDocumentRoot /var/www/$1.local\n\tErrorLog /var/www/logs/error.log\n\tCustomLog /var/www/logs/access.log combined\n\t<Directory "/var/www/$1.local">\n\t\tOrder allow,deny\n\t\tAllow from all\n\t\tRequire all granted\n\t\tAllowOverride All\n\t</Directory>\n</VirtualHost>" >> /etc/httpd/conf.d/000-$1.local.conf
+
+sudo echo -e "<VirtualHost *:443>\n\tServerAdmin tom@$1.local\n\tServerName $1.local\n\tServerAlias www.$1.local\n\tDocumentRoot /var/www/$1.local\n\tErrorLog /var/www/logs/error-ssl.log\n\tCustomLog /var/www/logs/access-ssl.log combined\n\t<Directory "/var/www/$1.local">\n\t\tOrder allow,deny\n\t\tAllow from all\n\t\tRequire all granted\n\t\tAllowOverride All\n\t</Directory>\n\tSSLEngine on\n\tSSLCertificateFile /var/www/ssl/server.crt\n\tSSLCertificateKeyFile /var/www/ssl/server.key\n</VirtualHost>" >> /etc/httpd/conf.d/000-ssl-$1.local.conf
+
 sudo chmod -R 777 /etc/httpd/conf.d/000-$1.local.conf
 sudo chown root:root /etc/httpd/conf.d/000-$1.local.conf
 sudo echo -e "To Infinity and beyond! Now just add '$(hostname -I)   $1.local' to your hosts file 🚀" >> /var/www/$1.local/index.html
@@ -68,11 +72,24 @@ echo -e "\n####### 8. NODE.JS ##############################\n"
 sudo curl -sL https://rpm.nodesource.com/setup_8.x | sudo bash -
 sudo yum install nodejs -y
 
+echo -e "\n####### 8. SSL ##############################\n"
+sudo mkdir /var/www/ssl
+sudo chmod -R 777 /var/www/ssl
+cd /var/www/ssl
+
+sudo echo -e "[req]\ndefault_bits = 2048\nprompt = no\ndefault_md = sha256\ndistinguished_name = dn\n\n[dn]\nC=GB\nST=$1\nL=$1\nO=$1\nOU=$1\nemailAddress=$1@email.com\nCN = $1.local" >> server.csr.cnf
+
+sudo echo -e "authorityKeyIdentifier=keyid,issuer\nbasicConstraints=CA:FALSE\nkeyUsage = digitalSignature, nonRepudiation, keyEncipherment, dataEncipherment\nsubjectAltName = @alt_names\n\n[alt_names]\nDNS.1 = $1.local" >> v3.ext
+
+openssl genrsa -des3 -out rootCA.key -passout pass:$1 2048
+
+openssl req -x509 -new -nodes -key rootCA.key -sha256 -days 1024 -passin pass:$1 -config <( cat server.csr.cnf ) -out rootCA.pem
+
+openssl req -new -sha256 -nodes -out server.csr -newkey rsa:2048 -keyout server.key -config <( cat server.csr.cnf )
+
+openssl x509 -req -in server.csr -CA rootCA.pem -CAkey rootCA.key -CAcreateserial -out server.crt -passin pass:$1 -days 500 -sha256 -extfile v3.ext
+
 echo -e "\n$1 was built! Now 'vagrant reload' to restart the box and get ready to develop 🦕\n"
 
 # CREATE DATABASE
 # XDEBUG
-# SETUP SSL
-
-
-
